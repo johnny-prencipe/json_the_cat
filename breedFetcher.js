1f246f67d75dbeb75335c3c98af5fdab19a486d8
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-03 23:01:26 +0000

    wrote general error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,8 @@ const catName = process.argv.splice(2).toString();
 
 request.get(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, function (error, response) {
   const data = JSON.parse(response.body);
-
+  
+  if (error) console.log('error:', error)
   if (data[0] !== undefined) {
     console.log(data[0].description)
   } else {
