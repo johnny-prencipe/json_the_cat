0352ea5b51d3ecf713965e11b622182ea1d853a4
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-03 22:59:18 +0000

    added functionality to search by cat name, added error handling for non existant cats

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,12 @@
 const request = require('request');
-request.get('https://api.thecatapi.com/v1/breeds/search/?q=sib', function (error, response) {
-  error ? console.log(error) : console.log(response.body);
+const catName = process.argv.splice(2).toString();
+
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, function (error, response) {
+  const data = JSON.parse(response.body);
+
+  if (data[0] !== undefined) {
+    console.log(data[0].description)
+  } else {
+    console.log(`Error: invalid cat name: ${catName} does not exist.`);
+  }
 })
-  // .on('response', function(response) {
-  //   console.log(body)
-  // });
