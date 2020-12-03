bb44844ea73006f902af6194ef7b24d83b7be12d
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-03 23:02:10 +0000

    ran ESlint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
 const catName = process.argv.splice(2).toString();
 
-request.get(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, function (error, response) {
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, function(error, response) {
   const data = JSON.parse(response.body);
   
-  if (error) console.log('error:', error)
+  if (error) console.log('error:', error);
   if (data[0] !== undefined) {
-    console.log(data[0].description)
+    console.log(data[0].description);
   } else {
     console.log(`Error: invalid cat name: ${catName} does not exist.`);
   }
-})
+});
